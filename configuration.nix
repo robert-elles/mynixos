475723855de6e12527b252ac198e4307c9d94486
Robert Elles <elles.robert@gmail.com>
2021-12-08 20:27:43 +0100

    watcher idea task neofetch

--- configuration.nix
+++ configuration.nix
@@ -263,6 +263,7 @@ in {
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     nano
+    neofetch
     nixfmt
     networkmanager
     curl
