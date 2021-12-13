f52801349326cde9b43998e147100fba0bd7bd03
Robert Elles <elles.robert@gmail.com>
2021-12-13 10:54:28 +0100

    pipewire pulse audio realtime config

--- configuration.nix
+++ configuration.nix
@@ -219,9 +219,9 @@ in {
         "link.max-buffers" = 16;
         "log.level" = 2;
         "default.clock.rate" = 48000;
-        "default.clock.quantum" = 32;
+        "default.clock.quantum" = 64;
         "default.clock.min-quantum" = 32;
-        "default.clock.max-quantum" = 32;
+        "default.clock.max-quantum" = 64;
         "core.daemon" = true;
         "core.name" = "pipewire-0";
       };
@@ -255,6 +255,40 @@ in {
         { name = "libpipewire-module-link-factory"; }
         { name = "libpipewire-module-session-manager"; }
       ];
+    };
+    config.pipewire-pulse = {
+      "context.properties" = { "log.level" = 2; };
+      "context.modules" = [
+        {
+          name = "libpipewire-module-rtkit";
+          args = {
+            "nice.level" = -15;
+            "rt.prio" = 88;
+            "rt.time.soft" = 200000;
+            "rt.time.hard" = 200000;
+          };
+          flags = [ "ifexists" "nofail" ];
+        }
+        { name = "libpipewire-module-protocol-native"; }
+        { name = "libpipewire-module-client-node"; }
+        { name = "libpipewire-module-adapter"; }
+        { name = "libpipewire-module-metadata"; }
+        {
+          name = "libpipewire-module-protocol-pulse";
+          args = {
+            "pulse.min.req" = "32/48000";
+            "pulse.default.req" = "32/48000";
+            "pulse.max.req" = "32/48000";
+            "pulse.min.quantum" = "32/48000";
+            "pulse.max.quantum" = "32/48000";
+            "server.address" = [ "unix:native" ];
+          };
+        }
+      ];
+      "stream.properties" = {
+        "node.latency" = "32/48000";
+        "resample.quality" = 1;
+      };
     };
   };
 
