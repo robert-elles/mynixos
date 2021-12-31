01db29d6cea8dfd47a6276828580ee57b13ddcf7
Robert Elles <elles.robert@gmail.com>
2021-12-31 09:55:10 +0100

    zsh-z plugin

--- configuration.nix
+++ configuration.nix
@@ -555,11 +555,13 @@ in {
       enable = true;
       zplug = {
         enable = true;
-        plugins = [{
-          name = "zsh-users/zsh-autosuggestions";
-        } # Simple plugin installation
-        #           { name = "romkatv/powerlevel10k"; tags = [ as:theme depth:1 ]; } # Installations with additional options. For the list of options, please refer to Zplug README.
-          ];
+        plugins = [
+          { name = "zsh-users/zsh-autosuggestions"; }
+          {
+            name = "agkozak/zsh-z";
+          }
+          #           { name = "romkatv/powerlevel10k"; tags = [ as:theme depth:1 ]; } # Installations with additional options. For the list of options, please refer to Zplug README.
+        ];
       };
       shellAliases = {
         ll = "ls -l";
