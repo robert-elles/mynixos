c286cd86d6f39dbc1bde9e4e4f072d29d2c63676
Robert Elles <elles.robert@gmail.com>
2022-01-13 15:26:59 +0100

    after suspend hook

--- hardware-configurations/rpi4b.nix
+++ hardware-configurations/rpi4b.nix
@@ -21,4 +21,6 @@
 
   # Enable GPU acceleration
   hardware.raspberry-pi."4".fkms-3d.enable = true;
+  hardware.raspberry-pi."4".audio.enable = true;
+  hardware.raspberry-pi."4".dwc2.enable = true;
 }
