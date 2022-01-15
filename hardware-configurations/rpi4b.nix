36a7af201112d71e4816abc66477890e13a5b698
Robert Elles <elles.robert@gmail.com>
2022-01-15 12:09:54 +0100

    mopidy and more raspi config

--- hardware-configurations/rpi4b.nix
+++ hardware-configurations/rpi4b.nix
@@ -22,5 +22,5 @@
   # Enable GPU acceleration
   hardware.raspberry-pi."4".fkms-3d.enable = true;
   hardware.raspberry-pi."4".audio.enable = true;
-  hardware.raspberry-pi."4".dwc2.enable = true;
+  #  hardware.raspberry-pi."4".dwc2.enable = true;
 }
--- machines/rpi4b.nix
+++ machines/rpi4b.nix
@@ -2,9 +2,48 @@
 let parameters = import ../parameters.nix;
 in {
 
+  boot.loader.grub.enable = false;
+  boot.loader.generic-extlinux-compatible.enable = true;
+  boot.loader.raspberryPi = {
+    enable = true;
+    uboot.enable = true;
+    version = 4;
+    #    firmwareConfig = ''
+    #      dtparam=audio=on
+    #    '';
+  };
+  boot = {
+    #    kernelPackages = pkgs.linuxPackages_latest;
+    kernelPackages = pkgs.linuxPackages_rpi4;
+    tmpOnTmpfs = true;
+    initrd.availableKernelModules = [ "usbhid" "usb_storage" ];
+    # ttyAMA0 is the serial console broken out to the GPIO
+    #    extraModprobeConfig = ''
+    #      options snd_bcm2835 enable_headphones=1
+    #    '';
+    kernelParams = [
+      "8250.nr_uarts=1"
+      "console=ttyAMA0,115200"
+      "console=tty1"
+      # Some gui programs need this
+      "cma=128M"
+    ];
+  };
+
+  # store journal in memory only
+  services.journald.extraConfig = ''
+    Storage = volatile
+    RuntimeMaxFileSize = 10M;
+  '';
+
   hardware.enableRedistributableFirmware = true;
   # Networking
   networking.hostName = "rpi4";
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  networking.firewall.enable = false;
   # Wireless
   networking = {
     wireless = {
@@ -26,15 +65,11 @@ in {
         #        Enable = "Source,Sink,Media,Socket";
         DiscoverableTimeout = 0;
         PairableTimeout = 0;
-        #        FastConnectable = true;
       };
     };
   };
-  #  sound.enable = true;
+  sound.enable = true;
   #  hardware.pulseaudio.enable = true;
-  boot.loader.raspberryPi.firmwareConfig = ''
-    dtparam=audio=on
-  '';
   # rtkit is optional but recommended
   security.rtkit.enable = true;
   services.pipewire = {
@@ -58,6 +93,95 @@ in {
     }];
   };
 
+  # mopidy
+  services.mopidy = {
+    enable = true;
+    extensionPackages = with pkgs; [
+      #      mopidy-spotify
+      mopidy-iris
+      mopidy-youtube
+      mopidy-soundcloud
+    ];
+    configuration = ''
+            #      [local]
+            #      enabled = true
+            #      media_dir = /data/nextcloud/nextcloud/data/robert/files/Music
+
+            [http]
+            enabled = true
+            hostname = 0.0.0.0
+            port = 6680
+
+            [spotify]
+            username = 1125705310
+            password = ${parameters.mopidy.spotify.pwd}
+            client_id = ${parameters.mopidy.spotify.client_id}
+            client_secret = ${parameters.mopidy.spotify.client_secret}
+            bitrate = 320
+
+            [audio]
+            #output = pulsesink server=127.0.0.1
+            #output = pulsesink device=1
+            #output = alsasink
+            #output = autoaudiosink
+            output = pulsesink
+            mixer = software
+
+            [youtube]
+            enabled = true
+            #autoplay_enabled = true
+            youtube_api_key = ${parameters.mopidy.youtube_api_key}
+            api_enabled = true
+
+            [mpd]
+            enabled = true
+            hostname = 0.0.0.0
+            zeroconf = mpd
+            port = 6610
+            connection_timeout = 240
+
+      #      [file]
+      #      enabled = true
+      #      media_dirs = /data/nextcloud/nextcloud/data/robert/files/Music
+      #      show_dotfiles = false
+      #      excluded_file_extensions =
+      #        .directory
+      #        .html
+      #        .jpeg
+      #        .jpg
+      #        .log
+      #        .nfo
+      #        .pdf
+      #        .png
+      #        .txt
+      #        .zip
+      #      follow_symlinks = false
+      #      metadata_timeout = 1000
+
+            [mowecl]
+            enabled = true
+
+            # generic config
+            seek_update_interval = 500
+            search_history_length = 10
+
+            # theme config
+            ## light or dark
+            theme_type = dark
+            #background_color = #fdf6e3
+            #text_color = #002b36
+            #primary_color = #268bd2
+
+
+            [soundcloud]
+            auth_token = ${parameters.mopidy.soundcloud_auth_token}
+            explore_songs = 25
+
+            [iris]
+            country = DE
+            locale = de_DE
+                      '';
+  };
   powerManagement.cpuFreqGovernor = "ondemand";
 
   home-manager.users.robert = {
@@ -67,7 +191,7 @@ in {
       zplug = {
         enable = true;
         plugins = [
-          { name = "zsh-users/zsh-autosuggestions"; }
+          #          { name = "zsh-users/zsh-autosuggestions"; }
           { name = "agkozak/zsh-z"; }
           { name = "agkozak/agkozak-zsh-prompt"; }
         ];
@@ -83,7 +207,12 @@ in {
     };
   };
 
-  environment.systemPackages = with pkgs; [ raspberrypi-eeprom libraspberrypi ];
+  environment.systemPackages = with pkgs; [
+    raspberrypi-eeprom
+    libraspberrypi
+    pamixer
+    pulseaudio
+  ];
 
   system.autoUpgrade.enable = true;
   system.autoUpgrade.allowReboot = true;
