2e9ce15235d473c635fc97bceb1f470a68275b58
Robert Elles <elles.robert@gmail.com>
2022-02-12 22:23:42 +0700

    bind

--- configuration.nix
+++ configuration.nix
@@ -57,6 +57,7 @@ in {
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
+    bind
     nano
     bash
     neofetch
