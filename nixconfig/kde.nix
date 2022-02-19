1f5c173df770b5c9dadbdb1732c0a7a80c77fa4c
Robert Elles <elles.robert@gmail.com>
2022-02-20 06:31:03 +0700

    sddm login manager with kde or i3 tilt 0.25.1 working kde connect

--- nixconfig/kde.nix
+++ nixconfig/kde.nix
@@ -1,24 +1,56 @@
-{ config, pkgs, ... }: {
+{ config, pkgs, unstable, ... }: {
 
   services.xserver = {
     enable = true;
     desktopManager.plasma5.enable = true;
     displayManager = {
       #      gdm.enable = true;
-      sddm.enable = true;
+      sddm = {
+        enable = true;
+        settings = {
+          General = {
+            type = "color";
+            color = "#242424";
+            background = null;
+          };
+        };
+      };
       defaultSession = "plasmawayland";
     };
-    #    windowManager.
+    windowManager.i3 = {
+      enable = true;
+      extraPackages = with pkgs; [
+        dmenu # application launcher most people use
+        i3status # gives you the default i3 status bar
+        i3lock # default i3 screen locker
+        i3blocks # if you are planning on using i3blocks over i3status
+      ];
+    };
   };
 
   programs.kdeconnect.enable = true;
-
+  programs.kdeconnect.package = unstable.libsForQt5.kdeconnect-kde;
   services.printing.enable = true;
+
+  services.xserver.libinput = {
+    enable = true;
+    touchpad = {
+      accelProfile = "flat";
+      accelSpeed = "1";
+    };
+    mouse = { accelSpeed = "1.2"; };
+  };
+  #services.xserver.libinput.mouse.accelProfile = adaptive;
+  services.unclutter.enable = true;
 
   environment.systemPackages = with pkgs; [
     libsForQt5.bismuth
     libsForQt5.kde-cli-tools
+    libsForQt5.ksystemlog
+    unstable.libsForQt5.kdeconnect-kde
     sshfs-fuse
+    sshfs
+    sftpman
   ];
 
   networking.firewall.allowedUDPPortRanges = [{
