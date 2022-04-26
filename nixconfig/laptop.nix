7018783bac48e8d0c8b7eceb849720a26b31affb
Robert Elles <elles.robert@gmail.com>
2022-04-26 06:26:24 +0200

    packages.nix

--- nixconfig/laptop.nix
+++ nixconfig/laptop.nix
@@ -5,6 +5,7 @@ in {
     (import ../config/btswitch/btswitch.nix)
     (import ./sound.nix)
     (import ./mediakeys.nix)
+    (import ./packages.nix)
     (import ./kde.nix {
       config = config;
       pkgs = pkgs;
@@ -99,141 +100,7 @@ in {
 
   services.openssh.allowSFTP = true;
 
-  environment.defaultPackages = with pkgs; [ keepassxc ];
-
-  environment.systemPackages = with pkgs; [
-    nixfmt
-    vlang
-    sshfs
-    networkmanager
-    inetutils
-    wireguard-tools
-    rofi # Window switcher, run dialog and dmenu replacement
-    arc-theme # gtk theme
-    lxappearance # set gtk themes
-    kde-gtk-config # should set kde themes
-    xarchiver
-    gnome.file-roller
-    cbatticon
-    gitAndTools.gitFull
-    # Foto
-    imagemagick
-    exiftool
-    mariadb
-    libraw
-    digikam
-    darktable
-    geeqie
-    glxinfo
-
-    ranger
-    handlr # set default applications
-    gparted
-    polkit_gnome # polkit authentication agent
-    feh
-
-    # Audio
-    pamixer
-    pulseaudio # needed for pactl
-    easyeffects
-    helvum # patchbay for pipewire
-    pavucontrol
-    playerctl
-    audio-recorder
-    audacity
-
-    nmap
-    mpv
-    gnome.gnome-clocks
-    rxvt-unicode
-    kitty
-    dunst
-    openvpn
-    p7zip
-    hydra-check
-    #    typora # markdown editor
-    auto-cpufreq
-    xorg.xev
-    light
-    apostrophe # markdown editor
-    #    wine
-    winetricks
-    wineWowPackages.stable
-    bottles
-    gnome.gnome-screenshot
-    libsecret
-    gnome.gnome-keyring
-    gnome.libgnome-keyring
-    xorg.xbacklight
-    xfce.xfce4-pulseaudio-plugin
-    xfce.thunar
-    xfce.xfconf # Needed to save the preferences
-    xfce.exo
-    xfce.thunar-archive-plugin
-    xfce.xfce4-i3-workspaces-plugin
-    xfce.xfce4-panel
-    xfce.xfce4-notifyd
-    xfce.xfce4-battery-plugin
-    xfce.xfce4-power-manager
-    # Development
-    tilt
-    #    ctlptl
-    glab # gitlab cli
-    jdk11
-    steam-run # run non-nixos compatible binaries
-    nodejs-14_x
-    maven
-    gradle
-    docker
-    docker-compose
-    pipenv
-    python38Packages.pip
-    python39Full
-    vscode
-    kube3d
-    pinta
-    postman
-    google-cloud-sdk
-    kubectl
-    kustomize
-
-    dbeaver
-    anki-bin
-    vulkan-tools
-    vulkan-loader
-    vulkan-headers
-    vulkan-validation-layers
-    mr
-    libva-utils
-    vdpauinfo
-    radeontop
-    arandr
-    autorandr
-    plasma-pa
-    firefox
-    joplin-desktop
-    #    unstable.chromium
-    chromium
-    #    unstable.zoom-us
-    zoom-us
-    vlc
-    spotify
-    transmission-gtk
-    blueberry
-    #    jetbrains.jdk
-    #    jetbrains.idea-ultimate
-    (jetbrains.idea-ultimate.override { jdk = pkgs.jetbrains.jdk; })
-    nextcloud-client
-    networkmanagerapplet
-    captive-browser
-    libreoffice-fresh
-    evince
-    gnome.gedit
-    multipath-tools # kpartx -av some_image.img creates device files that can be mounted
-    chiaki
-    tdrop
-    #     networkmanager_dmenu
-  ];
+  #    pkgs.anki-bin.overrideAttrs
 
   # tilt overlay for latest version
   nixpkgs.overlays = [
