614faac320418e6f094f3117edcac185dc228e33
Robert Elles <elles.robert@gmail.com>
2022-04-26 21:33:05 +0200

    working flake config

--- hardware-configurations/t495.nix
+++ hardware-configurations/t495.nix
@@ -5,17 +5,17 @@
   imports = [
     (modulesPath + "/installer/scan/not-detected.nix")
 
-    "${
-      builtins.fetchGit { url = "https://github.com/NixOS/nixos-hardware.git"; }
-    }/lenovo/thinkpad/t495"
-
-    "${
-      builtins.fetchGit { url = "https://github.com/NixOS/nixos-hardware.git"; }
-    }/common/gpu/amd"
-
-    "${
-      builtins.fetchGit { url = "https://github.com/NixOS/nixos-hardware.git"; }
-    }/common/pc/ssd"
+    #    "${
+    #      builtins.fetchGit { url = "https://github.com/NixOS/nixos-hardware.git"; }
+    #    }/lenovo/thinkpad/t495"
+    #
+    #    "${
+    #      builtins.fetchGit { url = "https://github.com/NixOS/nixos-hardware.git"; }
+    #    }/common/gpu/amd"
+    #
+    #    "${
+    #      builtins.fetchGit { url = "https://github.com/NixOS/nixos-hardware.git"; }
+    #    }/common/pc/ssd"
   ];
 
   boot.initrd.availableKernelModules =
