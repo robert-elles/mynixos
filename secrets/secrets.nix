6e8f40e63e3f05886f2e186f1504cfe9058e08be
Robert Elles <elles.robert@gmail.com>
2022-05-17 17:10:28 +0200

    wip: mopidy with age secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -5,4 +5,7 @@ let
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJH/vzGOac1ezLbdc5oyRzLU2I9F6SLt6wRAxItAXsZO";
   rpi4 =
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF9Nc0x2ejrf96rft9KiuJR5+hQqAqXZ+9H5JqS7JyTz";
-in { "wireless.env.age".publicKeys = [ panther robert rpi4 ]; }
+in {
+  "wireless.env.age".publicKeys = [ panther robert rpi4 ];
+  "mopidy_extra.conf.age".publicKeys = [ panther robert rpi4 ];
+}
