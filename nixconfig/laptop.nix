71fb5c7823dda97ee1ef67a20342beeaa9bc0e54
Robert Elles <elles.robert@gmail.com>
2022-09-28 13:02:19 +0200

    move deldockernet to scripts alias to get sha256

--- nixconfig/laptop.nix
+++ nixconfig/laptop.nix
@@ -147,6 +147,8 @@ in {
       };
       shellAliases = {
         ll = "ls -l";
+        getsha256 =
+          "nix-prefetch-url --type sha256 --unpack $1"; # $1 link to tar.gz release archive in github
         termcopy =
           "kitty +kitten ssh $1"; # copy terminal info to remote server $1 = remote server
         switch = "sudo nixos-rebuild -v switch --flake /etc/nixos/mynixos";
