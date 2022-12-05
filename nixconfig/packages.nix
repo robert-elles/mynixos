73440b8662e485da1f5ae0147a60de4ed96cb484
Robert Elles <elles.robert@gmail.com>
2022-12-05 19:46:15 +0100

    keybindings for vscode with jupyter python packages for notebooks in vscode

--- nixconfig/packages.nix
+++ nixconfig/packages.nix
@@ -69,6 +69,10 @@ let
       pyexiftool
       ebooklib
       beautifulsoup4
+      jupyter
+      pandas
+      numpy
+      matplotlib
       #      rich
     ];
   python-with-my-packages = python3.withPackages my-python-packages;
