3930226f23369fb45a7c554f587f4c5b1064bb8a
Robert Elles <elles.robert@gmail.com>
2023-01-31 15:02:59 +0100

    I need nvd only in the update script

--- nixconfig/laptop.nix
+++ nixconfig/laptop.nix
@@ -137,10 +137,10 @@ system_repo_root:
   # Or disable the firewall altogether.
   networking.firewall.enable = false;
 
-  system.activationScripts.report-changes = ''
-    PATH=$PATH:${lib.makeBinPath [ pkgs.nvd pkgs.nix ]}
-    nvd diff $(ls -dv /nix/var/nix/profiles/system-*-link | tail -2)
-  '';
+  # system.activationScripts.report-changes = ''
+  #   PATH=$PATH:${lib.makeBinPath [ pkgs.nvd pkgs.nix ]}
+  #   nvd diff $(ls -dv /nix/var/nix/profiles/system-*-link | tail -2)
+  # '';
 
   # run binaries compiled for other linux distributions
   programs.nix-ld.enable = true;
