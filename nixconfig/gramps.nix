4a4f33a536bb33eb5e512fa21dc2954b1c4353cc
Robert Elles <elles.robert@gmail.com>
2023-07-13 18:18:47 +0200

    first basic gramps-web service layout

--- nixconfig/gramps.nix
+++ nixconfig/gramps.nix
@@ -1,13 +1,16 @@
 { pkgs, ... }:
-let
-  grampsapi = pkgs.callPackage ../rpi4/gramps-webapi { };
-  grampsjs = pkgs.callPackage ../rpi4/gramps.js { };
-in
+# let
+# grampsapi = pkgs.callPackage ../rpi4/gramps-webapi { };
+# grampsjs = pkgs.callPackage ../rpi4/gramps.js { };
+# in
 {
-  # imports = [ ../rpi4/gramps-webapi ];
+  imports = [ ../rpi4/web-apps/gramps-web.nix ];
 
-  environment.systemPackages = [
-    grampsapi
-    grampsjs
-  ];
+  services.gramps-web.enable = true;
+  services.gramps-web.user = "robert";
+
+  # environment.systemPackages = [
+  #   grampsapi
+  #   grampsjs
+  # ];
 }
--- rpi4/web-apps/gramps-web.nix
+++ rpi4/web-apps/gramps-web.nix
@@ -1,14 +1,87 @@
-{ config, lib }:
+{ config, lib, ... }:
 let
-  cfg = config.services.calibre-web;
-  inherit (lib) mkIf mkEnableOption;
+  cfg = config.services.gramps-web;
+
+  inherit (lib) mkIf mkOption types mkEnableOption;
+  #   python = pkgs.python3;
 in
 {
   options = {
-    services.calibre-web = {
-      enable = mkEnableOption (lib.mdDoc "Calibre-Web");
+    services.gramps-web = {
+
+      enable = mkEnableOption (lib.mdDoc "Gramps-Web");
+
+      user = mkOption {
+        type = types.str;
+        default = "gramps-web";
+        description = lib.mdDoc "User account under which gramps-web runs.";
+      };
+
+      # group = mkOption {
+      #   type = types.str;
+      #   default = "gramps-web";
+      #   description = lib.mdDoc "Group account under which gramps-web runs.";
+      # };
+
+      # openFirewall = mkOption {
+      #   type = types.bool;
+      #   default = false;
+      #   description = lib.mdDoc ''
+      #     Open ports in the firewall for the server.
+      #   '';
+      # };
+
+      # dataDir = mkOption {
+      #   type = types.str;
+      #   default = "gramps-web";
+      #   description = lib.mdDoc ''
+      #     The directory below {file}`/var/lib` where gramps-web stores its data.
+      #   '';
+      # };
     };
   };
 
-  config = mkIf cfg.enable { };
+  config = mkIf cfg.enable {
+
+    # systemd.services.gramps-web = {
+    #   description = "Web app for collaborative genealogy";
+    #   after = [ "network.target" ];
+    #   wantedBy = [ "multi-user.target" ];
+
+    #   serviceConfig = {
+    #     Type = "simple";
+    #     User = cfg.user;
+    #     Group = cfg.group;
+
+    #     StateDirectory = cfg.dataDir;
+    #     # ExecStartPre = pkgs.writeShellScript "gramps-web-pre-start" (
+    #     #   ''
+    #     #     __RUN_MIGRATIONS_AND_EXIT=1 ${calibreWebCmd}
+
+    #     #     ${pkgs.sqlite}/bin/sqlite3 ${appDb} "update settings set ${settings}"
+    #     #   '' + optionalString (cfg.options.calibreLibrary != null) ''
+    #     #     test -f "${cfg.options.calibreLibrary}/metadata.db" || { echo "Invalid Calibre library"; exit 1; }
+    #     #   ''
+    #     # );
+
+    #     ExecStart = "${pkgs.${python}.gunikorn} -w 2 -b 0.0.0.0:5000";
+    #     Restart = "on-failure";
+    #   };
+    # };
+
+    # networking.firewall = mkIf cfg.openFirewall {
+    #   allowedTCPPorts = [ cfg.listen.port ];
+    # };
+
+    users.users = mkIf (cfg.user == "gramps-web") {
+      gramps-web = {
+        isSystemUser = true;
+        group = cfg.group;
+      };
+    };
+
+    # users.groups = mkIf (cfg.group == "gramps-web") {
+    #   gramps-web = { };
+    # };
+  };
 }
