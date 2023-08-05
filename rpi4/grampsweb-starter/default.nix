3faa2fc1b12e2806b8d592e4d86214f55e6cf322
Robert Elles <elles.robert@gmail.com>
2023-08-05 17:39:52 +0200

    clean up

--- rpi4/grampsweb-starter/default.nix
+++ rpi4/grampsweb-starter/default.nix
@@ -1,22 +1,13 @@
 { pkgs ? import <nixpkgs> { }, stdenv ? pkgs.stdenv, ... }:
 let
-  # script_ = pkgs.writeShellScriptBin "gramps_starter" ''
-  #   echo hello
-  # '';
-
   gramps-webapi = pkgs.callPackage ../gramps-webapi { };
-
   script = pkgs.writeShellApplication {
-
     name = "grampsweb_starter";
-    runtimeInputs = [ ];
     text = ''
       ${gramps-webapi.python.pkgs.gunicorn}/bin/gunicorn --workers=4 -b 0.0.0.0:5049 gramps_webapi.wsgi:app
     '';
   };
-
 in
-
 stdenv.mkDerivation {
   name = "grampsweb_starter";
   src = script;
