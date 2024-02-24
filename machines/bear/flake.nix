78463d2291a5de03a05b7bb934d003d5c553ed96
Robert Elles <4633229+robert-elles@users.noreply.github.com>
2024-02-24 17:58:25 +0100

    ide navigation possible

--- machines/bear/flake.nix
+++ machines/bear/flake.nix
@@ -21,9 +21,6 @@
       system = "x86_64-linux";
       system_repo_root = "/home/robert/code/mynixos";
       modules =
-        let
-          nixconfig = /. + system_repo_root + /nixconfig;
-        in
         [
           ({ ... }: with nixpkgs.lib; {
             options.mynix = {
@@ -47,19 +44,18 @@
             };
           })
           ({ ... }: {
+            networking.hostName = hostname; # Define your hostname.
             networking.firewall.enable = false;
             networking.extraHosts = ''
               192.168.178.69 falcon
             '';
-
-            networking.hostName = hostname; # Define your hostname.
           })
           agenix.nixosModules.default
-          (nixconfig + /hosts-blacklist)
-          (nixconfig + /laptop.nix)
-          (nixconfig + /dotfiles.nix)
-          (nixconfig + /common.nix)
-          (nixconfig + /pyenv.nix)
+          (../../nixconfig/hosts-blacklist)
+          (../../nixconfig/laptop.nix)
+          (../../nixconfig/dotfiles.nix)
+          (../../nixconfig/common.nix)
+          (../../nixconfig/pyenv.nix)
           (./hardware.nix)
         ];
     in
