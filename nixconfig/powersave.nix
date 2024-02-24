3c7abcde012cd631cd2b39aa0c25869739a784df
Robert Elles <4633229+robert-elles@users.noreply.github.com>
2024-02-24 13:56:03 +0100

    fix usb powersave

--- nixconfig/powersave.nix
+++ nixconfig/powersave.nix
@@ -4,13 +4,18 @@
     powerstat
     powertop
   ];
-  services.auto-cpufreq.enable = true; # auto-cpufreq --stats
+  services.auto-cpufreq.enable = false; # auto-cpufreq --stats
   powerManagement.powertop.enable = false;
   services.power-profiles-daemon.enable = false;
-  services.thermald.enable = true;
+  services.thermald.enable = false;
+  # swtich between battery and ac : sudo tlp ac | bat
+  # https://linrunner.de/tlp/support/troubleshooting.html
   services.tlp.enable = true; # tlp-stat -s
   services.tlp.settings = {
-    USB_AUTOSUSPEND = 0;
+    # USB_AUTOSUSPEND = 0;
+    # AHCI_RUNTIME_PM_ON_BAT = "on";
+    # RUNTIME_PM_DRIVER_DENYLIST = "ahci xhci_hcd";
+    RUNTIME_PM_BLACKLIST = "06:00.3 06:00.4"; # check with lspci
 
     # RADEON_DPM_PERF_LEVEL_ON_AC = "high";
     # RADEON_DPM_PERF_LEVEL_ON_BAT = "low";
