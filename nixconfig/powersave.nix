17cbe045f40e25f5e2697bb69af1a53bdc56781e
Robert Elles <4633229+robert-elles@users.noreply.github.com>
2024-02-25 14:52:07 +0100

    enable auto-cpufreq

--- nixconfig/powersave.nix
+++ nixconfig/powersave.nix
@@ -1,16 +1,16 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [
     powerstat
     powertop
   ];
-  services.auto-cpufreq.enable = false; # auto-cpufreq --stats
   powerManagement.powertop.enable = false;
   services.power-profiles-daemon.enable = false;
-  # services.thermald.enable = false;
+  services.auto-cpufreq.enable = true; # auto-cpufreq --stats
+  services.thermald.enable = config.services.auto-cpufreq.enable; # recommended by auto-cpufreq
   # swtich between battery and ac : sudo tlp ac | bat
   # https://linrunner.de/tlp/support/troubleshooting.html
-  services.tlp.enable = true; # tlp-stat -s
+  services.tlp.enable = !config.services.auto-cpufreq.enable; # tlp-stat -s
   services.tlp.settings = {
     # USB_AUTOSUSPEND = 0;
     RUNTIME_PM_BLACKLIST = "06:00.3 06:00.4"; # check with lspci
