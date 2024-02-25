4bac0165640fc8745a1cf58df99c0fce9065b411
Robert Elles <4633229+robert-elles@users.noreply.github.com>
2024-02-25 15:53:56 +0100

    powerManagement false

--- machines/falcon/flake.nix
+++ machines/falcon/flake.nix
@@ -54,6 +54,7 @@
             services.logind.lidSwitchExternalPower = "ignore";
             services.logind.lidSwitchDocked = "ignore";
             services.logind.lidSwitch = "ignore";
+            powerManagement.enable = false;
 
             systemd.additionalUpstreamSystemUnits = [ "debug-shell.service" ];
 
