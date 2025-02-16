a3c6c5abc2c7aa43d8cce67974ed0136958bc837
Robert Elles <4633229+robert-elles@users.noreply.github.com>
2025-02-16 12:07:58 +0100

    change shortcuts

--- nixconfig/plasma.nix
+++ nixconfig/plasma.nix
@@ -35,7 +35,26 @@
           "services/playerctl-4.desktop"."_launch" = "Ctrl+Shift+Space";
           "services/playerctl.desktop"."_launch" = "Ctrl+Shift+Right";
           "services/org.kde.spectacle.desktop"."RectangularRegionScreenShot" = [ "Meta+Shift+P" "Meta+Shift+Print" ];
-
+          "kwin"."Window Close" = [ "Meta+Shift+Q" "Alt+F4,Alt+F4,Close Window" ];
+          "kwin"."Switch to Desktop 1" = [ "Meta+1" "Ctrl+F1,Ctrl+F1,Switch to Desktop 1" ];
+          "kwin"."Switch to Desktop 2" = [ "Meta+2" "Ctrl+F2,Ctrl+F2,Switch to Desktop 2" ];
+          "kwin"."Switch to Desktop 3" = [ "Meta+3" "Ctrl+F3,Ctrl+F3,Switch to Desktop 3" ];
+          "kwin"."Switch to Desktop 4" = [ "Meta+4" "Ctrl+F4,Ctrl+F4,Switch to Desktop 4" ];
+          "kwin"."Switch to Desktop 5" = [ "Meta+5" "Ctrl+F5,Ctrl+F5,Switch to Desktop 5" ];
+          "kwin"."Switch to Desktop 6" = [ "Meta+6" "Ctrl+F6,Ctrl+F6,Switch to Desktop 6" ];
+          "kwin"."Switch to Desktop 7" = [ "Meta+7" "Ctrl+F7,Ctrl+F7,Switch to Desktop 7" ];
+          "kwin"."Switch to Desktop 8" = [ "Meta+8" "Ctrl+F8,Ctrl+F8,Switch to Desktop 8" ];
+          "kwin"."Switch to Desktop 9" = [ "Meta+9" "Ctrl+F9,Ctrl+F9,Switch to Desktop 9" ];
+          "kwin"."Switch to Desktop 10" = [ "Meta+0" "Ctrl+F10,Ctrl+F10,Switch to Desktop 10" ];
+          "plasmashell"."activate task manager entry 1" = "Ctrl+1,Meta+1,Activate Task Manager Entry 1";
+          "plasmashell"."activate task manager entry 2" = "Ctrl+2,Meta+2,Activate Task Manager Entry 2";
+          "plasmashell"."activate task manager entry 3" = "Ctrl+3,Meta+3,Activate Task Manager Entry 3";
+          "plasmashell"."activate task manager entry 4" = "Ctrl+4,Meta+4,Activate Task Manager Entry 4";
+          "plasmashell"."activate task manager entry 5" = "Ctrl+5,Meta+5,Activate Task Manager Entry 5";
+          "plasmashell"."activate task manager entry 6" = "Ctrl+6,Meta+6,Activate Task Manager Entry 6";
+          "plasmashell"."activate task manager entry 7" = "Ctrl+7,Meta+7,Activate Task Manager Entry 7";
+          "plasmashell"."activate task manager entry 8" = "Ctrl+8,Meta+8,Activate Task Manager Entry 8";
+          "plasmashell"."activate task manager entry 9" = "Ctrl+9,Meta+9,Activate Task Manager Entry 9";
         };
         configFile = {
           "kwinrc"."Desktops"."Id_10" = "cae9dbca-9e38-4abe-9ed5-7cdea84c8e1f";
@@ -49,6 +68,8 @@
           "kwinrc"."Desktops"."Id_9" = "064396a0-87a3-4e94-b1bd-2f827919b9a0";
           "kwinrc"."Desktops"."Number" = 10;
           "kwinrc"."Desktops"."Rows" = 1;
+          "kcminputrc"."Libinput/2/10/TPPS\\/2 Elan TrackPoint"."PointerAcceleration" = 0.000;
+          "kcminputrc"."Libinput/2/7/SynPS\\/2 Synaptics TouchPad"."PointerAcceleration" = 0.800;
         };
 
         panels = [
@@ -68,9 +89,7 @@
                   };
                   general = {
                     showWindowOutlines = true;
-                    displayedText = {
-                      desktopNumber = "Number";
-                    };
+                    displayedText = "desktopNumber";
                   };
                 };
               }
