b9a5055a525ef8552e482cf65db8683887bcba76
Robert Elles <4633229+robert-elles@users.noreply.github.com>
2025-07-05 13:47:02 +0200

    shortcuts for apps on mac

--- dotfiles/hammerspoon/init.lua
+++ dotfiles/hammerspoon/init.lua
@@ -1,9 +1,14 @@
 hs.hotkey.bind({"cmd"}, "e", function()
-  hs.execute("open -a Finder")
+  hs.execute("open ~/")
 end)
 hs.hotkey.bind({"cmd"}, "b", function()
   hs.execute("open -n -a Firefox")
 end)
 hs.hotkey.bind({"cmd"}, "return", function()
-  hs.execute("open -n -a Terminal")
+  hs.applescript.applescript([[
+    tell application "Terminal"
+      do script ""
+      activate
+    end tell
+  ]])
 end)
