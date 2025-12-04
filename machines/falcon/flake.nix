e2b0ed25e4da5390fe7a866b5d645f1bb420f3d3
Robert Elles <4633229+robert-elles@users.noreply.github.com>
2025-12-04 10:25:04 +0100

    working freshrss install

--- machines/falcon/flake.nix
+++ machines/falcon/flake.nix
@@ -225,7 +225,7 @@
         ../../nixconfig/server/wallabag.nix
         ../../nixconfig/server/mealie.nix
         ../../nixconfig/server/dawarich.nix
-        # ../../nixconfig/server/freshrss.nix
+        ../../nixconfig/server/freshrss.nix
         ../../nixconfig/server/vogesen.nix
       ];
     in
